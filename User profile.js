7d3dcfc49e9a002fef241566dcd9ba38f465ba69
Jude-raganit <juderaganit56@gmail.com>
2024-11-04 20:21:35 +0800

    Create User profile.js

---
+++ User profile.js
@@ -0,0 +1,8 @@
+let fullname= jude Lawrence raganit;
+console.log ('fullname')
+let age = 19;
+console.log('age')
+let favoritenumber= 18;
+console.log('favoritenumber')
+let favoritecolor = black;
+console.log('favoritecolor')
