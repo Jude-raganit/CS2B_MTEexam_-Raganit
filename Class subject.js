63dfc63382fea2caf3e03f80299c745f8557de83
Jude-raganit <juderaganit56@gmail.com>
2024-11-04 20:24:02 +0800

    Create Class subject.js

---
+++ Class subject.js
@@ -0,0 +1,8 @@
+let subjectTitle = "Data Structures and Algorithms";
+let classSchedule = "8:00 AM - 9:00 AM, Tuesday and Thursday";
+let classroom = "Comp lab";
+let classInstructor = "Vitruvius John Barayuga";
+let studentName = "Juan Paulo Passi";
+
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room  ${classroom}. The instructor for the subject is ${classInstructor}.`);
