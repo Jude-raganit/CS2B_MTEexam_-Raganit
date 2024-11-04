43cacb6f27420ee56f54e2cd62517468cc704c9a
Jude-raganit <juderaganit56@gmail.com>
2024-11-04 20:37:07 +0800

    Create Color reference.js

---
+++ Color reference.js
@@ -0,0 +1,9 @@
+
+let favoriteColors = [];
+for (let i = 0; i < 3; i++) {
+  let color = prompt(`Enter a color you like (${i + 1}/3):`);
+  
+  favoriteColors.push(color);
+
+  console.log("Updated color list:", favoriteColors);
+}
